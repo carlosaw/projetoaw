c381e951cd53e4ea571f3d9e44a68cb03d82d833
Your Name <you@example.com>
2021-07-06 10:31:03 -0400

    Comprar

--- src/components/Cart/Produto/Produto.js
+++ src/components/Cart/Produto/Produto.js
@@ -1,12 +1,17 @@
 import React from 'react';
+
+
 export class Produto extends React.Component {
+  adicionar = ()=> {
+    alert('clicou'+this.props.name);
+  }
   render() {
     return (
       <div className="produto">
-        <h4>{this.props.nome}</h4>
-        <h5>Estoque: {this.props.estoque}</h5>
+        <h4>{this.props.name}</h4>
+        <h5>Estoque: {this.props.stock}</h5>
 				<p>R$ {this.props.price}</p>	
-				<button>Adicionar ao Carrinho</button>
+				<button onClick={this.adicionar}>Adicionar ao Carrinho</button>
       </div>
     );
   }
@@ -17,10 +22,10 @@ export class Produtos extends React.Component {
     super(props);
     this.state = {
       produtos:[
-        {id:1, nome:'Teclado', estoque:10, price:199},
-        {id:2, nome:'Mouse', estoque:11, price:29},
-        {id:3, nome:'Monitor', estoque:12, price:399.00},
-        {id:4, nome:'CPU', estoque:13, price:49},
+        {id:1, name:'Teclado', stock:10, price:199},
+        {id:2, name:'Mouse', stock:11, price:29},
+        {id:3, name:'Monitor', stock:12, price:399.00},
+        {id:4, name:'CPU', stock:13, price:49},
       ]
     };
   }
@@ -29,7 +34,7 @@ export class Produtos extends React.Component {
     return (
     <div className="produtos">
       {this.state.produtos.map((item)=>
-        <Produto key={item.id} nome={item.nome} estoque={item.estoque} price={item.price} />
+        <Produto key={item.id} name={item.name} stock={item.stock} price={item.price} />
       )}
     </div>
     );
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -7,7 +7,7 @@ export class Cart extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			
 		};
 	}
 
